0bbfb1d6531f040f75183817a475106ce5a69516
woznin <szymonw037@gmail.com>
2023-11-30 00:46:57 +0100

    Zmiany front page

--- FUT Draft/FUT_Draft.py
+++ FUT Draft/FUT_Draft.py
@@ -1,9 +1,16 @@
 import tkinter as tk
+import ttkbootstrap as ttk
 
 
-window = tk.Tk()
-powitanie = tk.Label(text="Projekt Symulacja")
-powitanie.pack()
-window.mainloop()
-
-
+root = ttk.Window(themename='darkly')
+root.title('FUT Draft')
+root.geometry('800x900')
+for i in range(20):
+    root.columnconfigure(i, weight=1)
+    root.rowconfigure(i, weight=1)
+przywitanie = ttk.Label(root, text='Witamy w symulatorze', font='Calibri 24')
+przycisk_graj = ttk.Button(root, text='Symuluj!')
+przywitanie.grid(row=2, column=10)
+przycisk_graj.grid(row=8, column=8, rowspan=4, columnspan=3)
+root.resizable(False, False)
+root.mainloop()
